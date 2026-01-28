c411cebdf5700e53bde18dc29761c04e712315bb
Flora-went-skating <>
2026-01-28 22:24:18 +0000

    Challenge: Evaluating the Model with Cross-Validation

---
+++ modeling/challenge_evaluating_the_model_with_cross_validation/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+
+df = pd.read_csv('https://codefinity-content-media.s3.eu-west-1.amazonaws.com/a65bbc96-309e-4df9-a790-a1eb8c815a1c/penguins_pipelined.csv')
+# Assign X, y variables (X is already preprocessed and y is already encoded)
+X, y = df.drop('species', axis=1), df['species']
+# Initialize a model
+model = KNeighborsClassifier(4).fit(X, y) 
+# Calculate and print the mean of cross validation scores
+scores = cross_val_score(model, X, y, cv=3)
+#cross_val_score(KNeighborsClassifier(), X, y)
+print('Cross-val score:', scores.mean())
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+# Train a model
+model.fit(X_train, y_train)
+# Print the score using the test set
+print('Train-test score:', model.score(X_test, y_test))
